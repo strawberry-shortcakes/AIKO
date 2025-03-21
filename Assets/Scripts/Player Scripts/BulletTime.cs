4ea85501cac00c93ca779274a17a1e527fdce405
KannaAvery <koberudd62@gmail.com>
2025-03-22 05:19:48 +1100

    Sunrise Push
    - Added main menu music in menu scene and changed menu text to better reflect AIKO
    - Bullet time now produces a sound, but the sound currently loops until state is over
    - Kcene has a pause menu now
    - Kcene has flat white lighting
    - Random tune has been placed in Kannas test box
    - Hydrate

--- Assets/Scripts/Player Scripts/BulletTime.cs
+++ Assets/Scripts/Player Scripts/BulletTime.cs
@@ -2,20 +2,42 @@ using UnityEngine;
 
 public class BulletTime : MonoBehaviour
 {
-
     private float fixedDeltaTime;
-    // Update is called once per frame
+    public AudioSource bulletTimeSound; // Reference to an AudioSource that plays your Bullet Time sound
+    public AudioClip bulletTimeClip; // Assign your bullet time sound effect here
+
+    void Start()
+    {
+        // Set the sound to loop if you want it to loop during Bullet Time
+        bulletTimeSound.loop = true;
+    }
+
     void Update()
     {
         if (Input.GetKey(KeyCode.R))
         {
             if (Time.timeScale == 1.0f)
+            {
+                // Bullet Time: Slow down time and play sound at normal speed
                 Time.timeScale = 0.25f;
+                bulletTimeSound.clip = bulletTimeClip;
+                bulletTimeSound.Play();
+                bulletTimeSound.pitch = 1.0f;  // Maintain normal pitch
+            }
             else
+            {
+                // Normal Time: Reset back to normal speed and stop sound
                 Time.timeScale = 1.0f;
+                bulletTimeSound.Stop();
+            }
+
             // Adjust fixed delta time according to timescale
-            // The fixed delta time will now be 0.02 real-time seconds per frame
             Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
+        }
+        else if (Time.timeScale == 0.25f && !bulletTimeSound.isPlaying)
+        {
+            // Optional: Keep the sound playing if bullet time is still active but not repeating the key press
+            bulletTimeSound.Play();
         }
     }
 }
