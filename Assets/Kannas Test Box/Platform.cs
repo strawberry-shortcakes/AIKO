3d0673a93c68762b3d1091b4072d2bb0c2c64c45
KannaAvery <koberudd62@gmail.com>
2025-03-27 16:05:15 +1100

    PLATFORM COMPLETION

--- Assets/Kannas Test Box/Platform.cs
+++ Assets/Kannas Test Box/Platform.cs
@@ -7,10 +7,9 @@ public class Platform : MonoBehaviour
     public float _speed = 3.0f;
     private bool _switch = false;
 
-
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-
+        // Move the platform between position1 and position2
         if (_switch == false)
         {
             transform.position = Vector3.MoveTowards(transform.position, position1.position,
@@ -22,6 +21,7 @@ public class Platform : MonoBehaviour
                 _speed * Time.deltaTime);
         }
 
+        // Switch the direction when the platform reaches one of the positions
         if (transform.position == position1.position)
         {
             _switch = true;
@@ -30,4 +30,25 @@ public class Platform : MonoBehaviour
         {
             _switch = false;
         }
+    }
+
+    // When the player enters the platform, it will move with it
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) // Make sure the player has the "Player" tag
+        {
+            // Move the player along with the platform
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    // When the player leaves the platform, it will stop moving with it
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Remove the player from the platform's parent
+            collision.transform.SetParent(null);
+        }
+    }
 }
